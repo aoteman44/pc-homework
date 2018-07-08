3455874b38bc78944462fadc582718f1c5261b8a
超 潘 <wo8899174@hotmail.com>
2018-07-08 16:58:02 +0800

    初步的放大镜

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(43, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(375, 332);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.PictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.PictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(303, 141);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox2_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 430);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private bool blIsDrawRectangle = true;
+        private Point ptBegin = new Point();
+        Thread thDraw;
+        delegate void myDrawRectangel();
+        myDrawRectangel myDraw;
+
+        private Rectangle _SelectedRectangle;
+
+        PictureBox pb = new PictureBox();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void PictureBox1_Load(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+        }
+
+        private void Run()
+        {
+            while (isEnd)
+            {
+                if (pictureBox1.Image != null)
+                {
+                    this.BeginInvoke(myDraw);
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        private void ShowDrawRectangle()
+        {
+            Rectangle rec = new Rectangle(ptBegin.X * pictureBox1.Image.Size.Width / 375,
+                ptBegin.Y * pictureBox1.Image.Size.Height / 332, 50 * pictureBox1.Image.Size.Width / 375,
+                50 * pictureBox1.Image.Size.Height / 332);
+            Graphics g = pictureBox2.CreateGraphics();
+            g.DrawImage(pictureBox1.Image, pictureBox2.ClientRectangle, rec, GraphicsUnit.Pixel);
+            g.Flush();
+        }
+
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (blIsDrawRectangle)
+            {
+                e.Graphics.DrawArc(new Pen(Brushes.Black, 1), ptBegin.X, ptBegin.Y, 50, 50, 0, 360);
+            }
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            pictureBox2.Location = new Point(e.X + 50, e.Y - 100);
+
+            if (e.X - 25 <= 0)
+            {
+                ptBegin.X = 0;
+            }
+            else if (pictureBox1.Size.Width - e.X <= 25)
+            {
+                ptBegin.X = pictureBox1.Size.Width - 50;
+            }
+            else
+            {
+                ptBegin.X = e.X - 25;
+            }
+
+            if (e.Y - 25 <= 0)
+            {
+                ptBegin.Y = 0;
+            }
+            else if (pictureBox1.Size.Height - e.Y <= 25)
+            {
+                ptBegin.Y = pictureBox1.Size.Height - 50;
+            }
+            else
+            {
+                ptBegin.Y = e.Y - 25;
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private void PictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            blIsDrawRectangle = false;
+            pictureBox1.Refresh();
+        }
+
+        private void PictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            blIsDrawRectangle = true;
+        }
+
+
+        bool isEnd = false;
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isEnd = true;
+            if (isEnd)
+            {
+                pictureBox2.Visible = true;
+                myDraw = new myDrawRectangel(ShowDrawRectangle);
+                thDraw = new Thread(Run);
+                thDraw.Start();
+            }
+
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isEnd = false;
+            //pictureBox2.Refresh();
+            pictureBox2.Visible = false;
+        }
+
+        private void PictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            GraphicsPath gp = new GraphicsPath();
+
+            gp.AddEllipse(pictureBox2.ClientRectangle);
+
+            Region region = new Region(gp);
+
+            pictureBox2.Region = region;
+
+            var g = e.Graphics;
+            Pen pp = new Pen(Color.Blue, 5);
+
+            g.DrawEllipse(pp, pictureBox2.ClientRectangle);
+
+            gp.Dispose();
+
+            region.Dispose();
+        }
+    }
+}
+
